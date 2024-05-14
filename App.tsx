8b20172403bf763f6f393a69885dbd02fec1a2b1
Ariana <adelvalle@unlam.edu.ar>
2024-05-14 12:54:13 -0300

    Proyectito

---
+++ App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+  export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> PROYECTITO DE PRUEBA - PARA PRACTICAR REACT-NATIVEE *U* </Text>
+    
+    </View>
+  );
+
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
